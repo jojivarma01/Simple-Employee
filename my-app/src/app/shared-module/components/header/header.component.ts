7dc691f082c92e8b861cd1ead7486c5ee8248a8d
Jojivarma <jojivarma.gadiraju@netcracker.com>
2021-12-13 19:06:33 +0530

    added menu for login

--- my-app/src/app/shared-module/components/header/header.component.ts
+++ my-app/src/app/shared-module/components/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../../services/auth/auth-service.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit{
 
-  constructor() { }
+  isLogoutEnabled = false;
+  constructor(private router: Router,
+              private authService: AuthServiceService) { }
 
   ngOnInit(): void {
+    this.authService.$userData.subscribe((data) => {
+      if (Object.keys(data).length > 0) {
+        this.isLogoutEnabled = true;
+      } else {
+        this.isLogoutEnabled = false;
+      }
+    })
+  }
+
+  onLogout() {
+    this.authService.setTimerOut();
+  }
+
+  onLogin() {
+    this.router.navigate(['login']);
   }
 
 }
