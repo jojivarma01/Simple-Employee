84be536bc93322ace542a140b5785e7028185605
Jojivarma <jojivarma.gadiraju@netcracker.com>
2021-12-15 15:51:50 +0530

    returned observable from guards

--- my-app/src/app/shared-module/services/auth-guards/auth-guard.service.ts
+++ my-app/src/app/shared-module/services/auth-guards/auth-guard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { AuthServiceService } from '../auth/auth-service.service';
 
 @Injectable({
@@ -10,27 +10,19 @@ export class AuthGuardService implements CanLoad {
 
   constructor(private userData: AuthServiceService) { }
 
-  canLoad(route: Route, segments: UrlSegment[]): boolean {
-    let canAccessRoute = false;
-    this.userData.$userData.subscribe((loggedInUserData) => {
-      if (loggedInUserData && loggedInUserData.isLoginSuccess) {
-        canAccessRoute = true;
-      }
-    });
-
-    return canAccessRoute;
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+    return this.userData.$userData.pipe(
+      map((data) => {
+        if(data && data.isLoginSuccess) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean  {
-    let canAccessRoute = false;
-    console.log('route-', route);
-    console.log('state-', state);
-    this.userData.$userData.subscribe((loggedInUserData) => {
-      if (loggedInUserData && loggedInUserData.isLoginSuccess) {
-        canAccessRoute = true;
-      }
-    });
-
-    return canAccessRoute;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    return this.userData.$userData.getValue().isLoginSuccess; 
   }
 }
